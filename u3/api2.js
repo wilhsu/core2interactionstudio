8da5158466e7d07bbf523b5201f2ccc1d40f1041
wilhsu <80747302+wilhsu@users.noreply.github.com>
2022-04-25 13:51:01 -0400

    Update api2.js

--- u3/api2.js
+++ u3/api2.js
@@ -43,6 +43,8 @@ async function main(){
           console.log(modelName);
           document.querySelector(".textah").append(modelName);
 
+          let ring = document.createElement("audio");
+          document.getElementsByTagName("audio");
           let manufacturers = apiData.records[i].fields.manufacturer;
           Audio.src = 
           'assets/audio/' + 
@@ -54,48 +56,3 @@ async function main(){
 
 
 main();
-
-// // var airtable = require("airtable");
-// // var base = new airtable({ apiKey: 'keyRsJYzEsQrpUF9n'}).base('tblb85lWYZDPksPv2'
-// // );
-
-// // base("XD").select()
-
-// //the html elements we want to access
-// const image_container = document.getElementById("image_container");
-
-// //fetching the data from airtable base
-// async function fetchData(url){
-//   let response = await fetch(url);   //server sends a response
-//   let jsonData = await response.json();   //convert the response into a json format
-//   return jsonData; //this line returns a json object 
-// }
-
-// //storing that fetched data into our array 'apiData'
-// async function getData(){
-//   let data = await fetchData(apiURL); //call the fetchData function and store json in 'data' variable
-
-//   //iterate over the above data and pull out only the required fields. 
-//   //we dont need all the extra info like the field id and all that Airtable returns
-//   //so instead, we will only store our main content which lives in records.fields
-//   for(let i =0; i<data.records.length; i++){
-//     let record = data.records[i].fields;
-//     apiData.push(record); //push those records into apiData to make a clean array
-
-//         // let model = apiData.records[i].fields.model;
-//         // let heading = document.createElement("h2");
-//         // heading.innerHTML = model;
-//         // document.body.appendChild(heading);
-        
-//         let image = document.createElement("img");
-//         image.src = apiData.records[i].fields.img.url;
-//         image.className = "images" + i;
-//         document.body.appendChild(image);
-
-//         // let man = document.createElement("h3");
-//         // man.innerHTML = apiData.records[i].fields.manufacturer;
-//         // document.body.appendChild(man);
-//   }
-// }
-
-// getData();
